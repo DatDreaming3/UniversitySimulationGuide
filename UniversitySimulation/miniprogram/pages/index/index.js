b57cdc17e30742d1bf76b22dba0ce6f29c21887f
while101 <90126996+while101@users.noreply.github.com>
2021-11-27 19:47:06 +0800

    增加提交数据上传到数据库的功能

--- UniversitySimulation/miniprogram/pages/index/index.js
+++ UniversitySimulation/miniprogram/pages/index/index.js
@@ -1,7 +1,21 @@
 const db = wx.cloud.database();
  Page({
   data: {
+    dataObj:""
   },
-  addData(r){
+  addData:function(res){
+    var{event}=res.detail.value;  
+      wx.showLoading({
+        title:"提交中...",
+        mask:true
+      })
+      db.collection("list").add({
+        data:{
+          event:event
+        }
+      }).then(res=>{
+        console.log(res) 
+        wx.hideLoading()
+      })
   }
 })
